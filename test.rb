6e8d70c204b922cffb930b9ac63b74ba6fd45996
gabriele renzi <rff.rff@gmail.com>
2010-01-24 14:00:03 +0100

    support no arguments

--- test.rb
+++ test.rb
@@ -5,11 +5,11 @@ class T < Test::Unit::TestCase
   def test_samples
     examples = File.readlines('jeg').grep(/jeg .*=>/)
     examples.each do |line|
-      p line
+      #p line
       cmd, exp = line.split('=>') 
       json, pipe = cmd.split('|')
       res = %x{echo '#{json}' | #{pipe}}
-      print 'json:'+json, 'pipe:'+pipe, 'exp:'+exp, 'res:'+res, "\n"
+      #print 'json:'+json, 'pipe:'+pipe, 'exp:'+exp, 'res:'+res, "\n"
       assert_equal JSON.parse(exp.strip), JSON.parse(res.strip),"#{json} failed"
     end
   end
