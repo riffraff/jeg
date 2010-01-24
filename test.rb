3a4dfb157e6df9c7692199655d977f47a5e0d947
gabriele renzi <rff.rff@gmail.com>
2010-01-24 13:51:23 +0100

    some functional tests dded

---
+++ test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+require 'rubygems'
+require 'json'
+class T < Test::Unit::TestCase
+  def test_samples
+    examples = File.readlines('jeg').grep(/jeg .*=>/)
+    examples.each do |line|
+      p line
+      cmd, exp = line.split('=>') 
+      json, pipe = cmd.split('|')
+      res = %x{echo '#{json}' | #{pipe}}
+      print 'json:'+json, 'pipe:'+pipe, 'exp:'+exp, 'res:'+res, "\n"
+      assert_equal JSON.parse(exp.strip), JSON.parse(res.strip),"#{json} failed"
+    end
+  end
+end
+
