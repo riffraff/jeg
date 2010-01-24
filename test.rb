fe31f00bd39ecb0c4f7d2170ddae339fcdbaa039
gabriele renzi <rff.rff@gmail.com>
2010-01-24 16:50:56 +0100

    allow deciding he output format in a saner way, make smart defaul and more useful

--- test.rb
+++ test.rb
@@ -6,11 +6,17 @@ class T < Test::Unit::TestCase
     examples = File.readlines('jeg').grep(/jeg .*=>/)
     examples.each do |line|
       #p line
-      cmd, exp = line.split('=>') 
+      cmd, exp = line.scan(%r-(.*?)=>(.*)-)[0] 
       json, pipe = cmd.split('|')
       res = %x{echo '#{json}' | #{pipe}}
       #print 'json:'+json, 'pipe:'+pipe, 'exp:'+exp, 'res:'+res, "\n"
-      assert_equal JSON.parse(exp.strip), JSON.parse(res.strip),"#{json} failed"
+      if pipe =~ /-f json/
+        assert_equal JSON.parse(exp.strip), JSON.parse(res.strip),"#{json} failed with cmd #{cmd}"
+      elsif pipe =~ /-f ruby/
+        assert_equal eval(exp.strip), eval(res.strip),"#{json} failed with cmd #{cmd}"
+      else
+        assert_equal exp.strip, res.strip,"#{json} failed with cmd #{cmd}"
+      end
     end
   end
 end
